a80756a4ac5df2b33baab2608de0103d47de8d38
gobrien06 <gobrien088@gmail.com>
2020-10-09 15:25:23 -0700

    Styling account and login screens. New animations.

--- src/Components/GlanceHeader/CompareChart.js
+++ src/Components/GlanceHeader/CompareChart.js
@@ -43,26 +43,6 @@ class CompareChart extends Component {
         animate={true}
         motionStiffness={90}
         motionDamping={15}
-        defs={[
-          {
-            id: "dots",
-            type: "patternDots",
-            background: "inherit",
-            color: "rgba(255, 255, 255, 0.3)",
-            size: 4,
-            padding: 1,
-            stagger: true,
-          },
-          {
-            id: "lines",
-            type: "patternLines",
-            background: "inherit",
-            color: "rgba(255, 255, 255, 0.3)",
-            rotation: -45,
-            lineWidth: 6,
-            spacing: 10,
-          },
-        ]}
         legends={[
           {
             anchor: "bottom",
